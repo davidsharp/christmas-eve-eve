0ed5c88ff1cf3f8b576bff80338ffb30bf4ae1f7
Glitch (hello-webpage) <none>
2019-01-02 13:17:16 +0000

    🚿🏟 Checkpoint ./script.js:389362/765 ./index.html:389362/48 ./style.css:389362/39 ./README.md:389362/1094

--- script.js
+++ script.js
@@ -1,7 +1,10 @@
-//console.log(window['christmas-eve-eve'])
-
 const xmaseve = window['christmas-eve-eve']
 
 const now = luxon.DateTime.local();
 let xmas = now.set({month:12, day:25})
 if(xmas<now)xmas=xmas.set({year:xmas.year+1})
+
+const days = Math.floor(xmas.diff(now, 'days').toObject().days)
+xmaseve.innerText = `Today is Christmas${Array(days).fill(' Eve').join('')}!`
+//xmaseve.setAttribute("alt", `(That's ${days} days until Christmas)`);
+window['how-many-days'].innerText = `(That's ${days} days until Christmas)`
