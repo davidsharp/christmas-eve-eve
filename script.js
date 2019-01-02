2073fa07d0c6f8a342fe723544fc224d9194de06
Glitch (hello-webpage) <none>
2019-01-02 13:06:16 +0000

    🐞🐡 Checkpoint ./.glitch-assets:389362/410 ./style.css:389362/714 ./script.js:389362/269

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
-console.log(window['christmas-eve-eve'])
+//console.log(window['christmas-eve-eve'])
+
+const xmaseve = window['christmas-eve-eve']
+
+const now = luxon.DateTime.local();
+let xmas = now.set({month:12, day:25})
+if(xmas<now)xmas=xmas.set({year:xmas.year+1})
