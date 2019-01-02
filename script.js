342d38f427362c5c1f1f2d7daced426e60378f50
Glitch (hello-webpage) <none>
2019-01-02 13:39:18 +0000

    😋🏞 Checkpoint ./style.css:389362/244 ./.glitch-assets:389362/388 ./script.js:389362/28 ./index.html:389362/96

--- script.js
+++ script.js
@@ -5,6 +5,6 @@ let xmas = now.set({month:12, day:25})
 if(xmas<now)xmas=xmas.set({year:xmas.year+1})
 
 const days = Math.floor(xmas.diff(now, 'days').toObject().days)
-xmaseve.innerText = `Today is Christmas${Array(days).fill(' Eve').join('')}!`
+xmaseve.innerText = `Today is Christmas${Array(days).fill(' Eve').join('')}!`.toUpperCase()
 //xmaseve.setAttribute("alt", `(That's ${days} days until Christmas)`);
-window['how-many-days'].innerText = `(That's ${days} days until Christmas)`
+window['how-many-days'].innerText = `(That's ${days} days until Christmas)`.toUpperCase()
